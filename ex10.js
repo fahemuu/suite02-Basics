a2a568f428f62289c832fce8efcb352437a678cf
fahemuu <fahem.id@gmail.com>
2017-04-13 09:45:37 +0200

    exo10

--- ex10.js
+++ ex10.js
@@ -16,3 +16,35 @@
         Array.sort
 
 */
+function sortNumbers(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+                var temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+function concatArrays(arr1, arr2) {
+    var a = arr1;
+    const taille1 = arr1.length
+    const taille2 = arr2.length
+    for (var i = 0; i < taille2; i++) {
+        a[taille1 + i] = arr2[i];
+    }
+    return a;
+}
+
+
+function doubleSortNumbers(arr1, arr2) {
+    arr = concatArrays(arr1, arr2);
+    arr= sortNumbers(arr);
+
+}
