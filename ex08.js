bf24575e63367c31569e3409b466422d96acd32b
fahemuu <fahem.id@gmail.com>
2017-04-13 09:29:14 +0200

    exo7-8

--- ex08.js
+++ ex08.js
@@ -1,5 +1,4 @@
 /*
-
     3D array
 
     Ecrire une fonction "getValuesFrom3DArray", qui va mettre dans un tableau à une dimension chaque valeurs trouvé.
@@ -11,6 +10,9 @@
         getValuesFrom3DArray([
             [
                 ["Oulala", "ça", "fait"],
+                  [
+                ["Pourtant c'est la même chose quasiment !"]
+            ]
                 ["beaucoup", "de", "crochets!!!"]
             ],
             [
@@ -20,3 +22,25 @@
         // ["Oulala", "ça", "fait", "beaucoup", "de", "crochets!!!", "Pourtant c'est la même chose quasiment !"]
 
 */
+
+//à améliorer 
+function getValuesFrom3DArray(arr) {
+
+    var arrFinal = new Array;
+    const nbRow = arr.length
+    var curentRow
+
+    for (var row = 0; row < nbRow; row++) {
+
+        curentRow = arr[row]
+        for (var col = 0; col < curentRow.length; col++) {
+
+            curentRow[col].forEach(function (element) {
+                arrFinal.push(element)
+            }, this);
+
+        }
+
+    }
+    return arrFinal;
+}
